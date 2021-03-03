b3fa949f23898e2456b65d2c77e62a3662b2ef31
Aaron Ojala <aaronojala@yahoo.com>
2021-03-03 23:50:49 +0200

    test with post method

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 
 app.use('/static', express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
     res.render('todo.ejs');
 });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+})
 
 app.listen(3000, () => console.log('Server up and running'));
